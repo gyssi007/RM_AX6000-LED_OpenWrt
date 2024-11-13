053ef121b8a337fabb5c4e0841f22e2ca6e6ac4e
gyssi007 <78358246+gyssi007@users.noreply.github.com>
2024-11-14 04:30:32 +0800

    Update diy-part2.sh

--- padavanonly/Redmi-AX6000/diy-part2.sh
+++ padavanonly/Redmi-AX6000/diy-part2.sh
@@ -19,4 +19,6 @@ chmod +x /tmp/clash >/dev/null 2>&1
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+echo "src/gz kenzok8 https://op.dllkids.xyz/packages/aarch64_cortex-a53" >> /etc/opkg/customfeeds.conf
+sed -i 's/^option check_signature/#option check_signature/' /etc/opkg.conf
 ##---------------------------------------------------------
